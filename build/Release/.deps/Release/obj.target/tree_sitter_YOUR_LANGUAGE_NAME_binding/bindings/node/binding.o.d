ebea07e3b8e6b750111c7084e9022c622e9862e0
Joscha Loos <j.loos@posteo.net>
2023-03-26 10:25:02 +0200

    build?

---
+++ build/Release/.deps/Release/obj.target/tree_sitter_YOUR_LANGUAGE_NAME_binding/bindings/node/binding.o.d
@@ -0,0 +1,155 @@
+cmd_Release/obj.target/tree_sitter_YOUR_LANGUAGE_NAME_binding/bindings/node/binding.o := g++ -o Release/obj.target/tree_sitter_YOUR_LANGUAGE_NAME_binding/bindings/node/binding.o ../bindings/node/binding.cc '-DNODE_GYP_MODULE_NAME=tree_sitter_YOUR_LANGUAGE_NAME_binding' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-DV8_DEPRECATION_WARNINGS' '-DV8_IMMINENT_DEPRECATION_WARNINGS' '-D_GLIBCXX_USE_CXX11_ABI=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-D__STDC_FORMAT_MACROS' '-DBUILDING_NODE_EXTENSION' -I/nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node -I/nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/src -I/nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/deps/openssl/config -I/nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/deps/openssl/openssl/include -I/nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/deps/uv/include -I/nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/deps/zlib -I/nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/deps/v8/include -I../node_modules/nan -I../src  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -O3 -fno-omit-frame-pointer -fno-rtti -fno-exceptions -std=gnu++17 -MMD -MF ./Release/.deps/Release/obj.target/tree_sitter_YOUR_LANGUAGE_NAME_binding/bindings/node/binding.o.d.raw   -c
+Release/obj.target/tree_sitter_YOUR_LANGUAGE_NAME_binding/bindings/node/binding.o: \
+ ../bindings/node/binding.cc ../src/tree_sitter/parser.h \
+ /nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/node.h \
+ /nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8.h \
+ /nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/cppgc/common.h \
+ /nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8config.h \
+ /nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-array-buffer.h \
+ /nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-local-handle.h \
+ /nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-internal.h \
+ /nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-version.h \
+ /nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8config.h \
+ /nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-object.h \
+ /nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-maybe.h \
+ /nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-persistent-handle.h \
+ /nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-weak-callback-info.h \
+ /nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-primitive.h \
+ /nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-data.h \
+ /nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-value.h \
+ /nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-traced-handle.h \
+ /nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-container.h \
+ /nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-context.h \
+ /nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-snapshot.h \
+ /nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-date.h \
+ /nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-debug.h \
+ /nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-script.h \
+ /nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-message.h \
+ /nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-exception.h \
+ /nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-extension.h \
+ /nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-external.h \
+ /nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-function.h \
+ /nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-function-callback.h \
+ /nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-template.h \
+ /nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-memory-span.h \
+ /nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-initialization.h \
+ /nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-callbacks.h \
+ /nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-isolate.h \
+ /nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-embedder-heap.h \
+ /nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-microtask.h \
+ /nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-statistics.h \
+ /nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-promise.h \
+ /nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-unwinder.h \
+ /nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-embedder-state-scope.h \
+ /nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-platform.h \
+ /nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-json.h \
+ /nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-locker.h \
+ /nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-microtask-queue.h \
+ /nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-primitive-object.h \
+ /nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-proxy.h \
+ /nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-regexp.h \
+ /nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-typed-array.h \
+ /nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-value-serializer.h \
+ /nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-wasm.h \
+ /nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/node_version.h \
+ ../node_modules/nan/nan.h \
+ /nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/node_version.h \
+ /nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/uv.h \
+ /nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/uv/errno.h \
+ /nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/uv/version.h \
+ /nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/uv/unix.h \
+ /nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/uv/threadpool.h \
+ /nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/uv/linux.h \
+ /nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/node_buffer.h \
+ /nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/node.h \
+ /nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/node_object_wrap.h \
+ ../node_modules/nan/nan_callbacks.h \
+ ../node_modules/nan/nan_callbacks_12_inl.h \
+ ../node_modules/nan/nan_maybe_43_inl.h \
+ ../node_modules/nan/nan_converters.h \
+ ../node_modules/nan/nan_converters_43_inl.h \
+ ../node_modules/nan/nan_new.h \
+ ../node_modules/nan/nan_implementation_12_inl.h \
+ ../node_modules/nan/nan_persistent_12_inl.h \
+ ../node_modules/nan/nan_weak.h ../node_modules/nan/nan_object_wrap.h \
+ ../node_modules/nan/nan_private.h \
+ ../node_modules/nan/nan_typedarray_contents.h \
+ ../node_modules/nan/nan_json.h ../node_modules/nan/nan_scriptorigin.h
+../bindings/node/binding.cc:
+../src/tree_sitter/parser.h:
+/nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/node.h:
+/nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8.h:
+/nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/cppgc/common.h:
+/nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8config.h:
+/nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-array-buffer.h:
+/nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-local-handle.h:
+/nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-internal.h:
+/nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-version.h:
+/nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8config.h:
+/nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-object.h:
+/nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-maybe.h:
+/nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-persistent-handle.h:
+/nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-weak-callback-info.h:
+/nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-primitive.h:
+/nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-data.h:
+/nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-value.h:
+/nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-traced-handle.h:
+/nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-container.h:
+/nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-context.h:
+/nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-snapshot.h:
+/nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-date.h:
+/nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-debug.h:
+/nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-script.h:
+/nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-message.h:
+/nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-exception.h:
+/nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-extension.h:
+/nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-external.h:
+/nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-function.h:
+/nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-function-callback.h:
+/nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-template.h:
+/nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-memory-span.h:
+/nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-initialization.h:
+/nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-callbacks.h:
+/nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-isolate.h:
+/nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-embedder-heap.h:
+/nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-microtask.h:
+/nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-statistics.h:
+/nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-promise.h:
+/nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-unwinder.h:
+/nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-embedder-state-scope.h:
+/nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-platform.h:
+/nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-json.h:
+/nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-locker.h:
+/nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-microtask-queue.h:
+/nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-primitive-object.h:
+/nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-proxy.h:
+/nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-regexp.h:
+/nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-typed-array.h:
+/nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-value-serializer.h:
+/nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/v8-wasm.h:
+/nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/node_version.h:
+../node_modules/nan/nan.h:
+/nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/node_version.h:
+/nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/uv.h:
+/nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/uv/errno.h:
+/nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/uv/version.h:
+/nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/uv/unix.h:
+/nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/uv/threadpool.h:
+/nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/uv/linux.h:
+/nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/node_buffer.h:
+/nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/node.h:
+/nix/store/xi85xjr9m7141z61r954z47jnpcsjq9s-nodejs-18.14.2/include/node/node_object_wrap.h:
+../node_modules/nan/nan_callbacks.h:
+../node_modules/nan/nan_callbacks_12_inl.h:
+../node_modules/nan/nan_maybe_43_inl.h:
+../node_modules/nan/nan_converters.h:
+../node_modules/nan/nan_converters_43_inl.h:
+../node_modules/nan/nan_new.h:
+../node_modules/nan/nan_implementation_12_inl.h:
+../node_modules/nan/nan_persistent_12_inl.h:
+../node_modules/nan/nan_weak.h:
+../node_modules/nan/nan_object_wrap.h:
+../node_modules/nan/nan_private.h:
+../node_modules/nan/nan_typedarray_contents.h:
+../node_modules/nan/nan_json.h:
+../node_modules/nan/nan_scriptorigin.h:
